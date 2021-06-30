4f9bf8800c7adf090190cb9df151d7b4548a0ea1
User <user@Users-iMac.local>
2021-06-30 11:02:18 -0700

    first command

---
+++ customers.py
@@ -0,0 +1,13 @@
+import pyautogui as pyg
+
+pyg.FAILSAFE = True
+
+y = 1
+pyg.moveTo(18,352,0.1)
+pyg.click()
+pyg.moveTo(18,352,0.1)
+pyg.click()
+print("devyn is coding")
+#pyg.moveTo(469, 45, 0.1)
+pyg.moveTo(21,374,0.1)
+
